8b87b61e5fba78158482de54c677a9e9014c5b47
Max Meinel <max.meinel@gmx.net>
2022-02-03 10:46:54 +0100

    array umbenannt + kommentare

--- Quicksort.java
+++ Quicksort.java
@@ -3,66 +3,66 @@ import java.util.Random;
 public class Quicksort {
 
     public static void main(String[] args) {
-        //elements ist das Array, welches sortiert werden soll / es hat 10 items
-        int[] elements = new int[10];
+        //array ist das Array, welches sortiert werden soll / es hat 10 items
+        int[] array = new int[10];
 
-        //elements bekommt zufällige Zahlen an alle Stellen des Arrays eingetragen
+        //array bekommt zufällige Zahlen an alle Stellen des Arrays eingetragen
         Random rn = new Random();
-        for (int i = 0; i < elements.length; i++) {
-            elements[i] = rn.nextInt(20);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rn.nextInt(20);
         }
 
         //Durch eine for-Schleife wird das Array einmal vor dem sortieren ausgegeben
-        for (int i = 0; i < elements.length; i++) {
-            System.out.print(elements[i] + " ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        //der Quicksort sortieralgorythmus wird aufgerufen und das unsortierte Array "elements" wird übergeben
-        sort(elements);
+        //der Quicksort sortieralgorythmus wird aufgerufen und das unsortierte Array "array" wird übergeben
+        sort(array);
 
-        //nachdem die Liste sortiert wurde, wird das sortierte Array ausgegeben
-        for (int i = 0; i < elements.length; i++) {
-            System.out.print(elements[i] + " ");
+        //nachdem das Array sortiert wurde, wird es sortiert ausgegeben
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 
-    public static void sort(int[] elements) {
-        //
-        quicksort(elements, 0, elements.length - 1);
+    public static void sort(int[] array) {
+        //das Array wird dem "quicksort" übergeben / beim 1. Aufruf ist das Array noch nicht geteilt, deswegen wird von 0 - letztes Element übergeben
+        quicksort(array, 0, array.length - 1);
     }
 
-    private static void quicksort(int[] elements, int left, int right) {
+    private static void quicksort(int[] array, int left, int right) {
         // End of recursion reached?
         if (left >= right) return;
 
-        int pivotPos = partition(elements, left, right);
-        quicksort(elements, left, pivotPos - 1);
-        quicksort(elements, pivotPos + 1, right);
+        int pivotPos = partition(array, left, right);
+        quicksort(array, left, pivotPos - 1);
+        quicksort(array, pivotPos + 1, right);
     }
 
-    public static int partition(int[] elements, int left, int right) {
-        int pivot = elements[right];
+    public static int partition(int[] array, int left, int right) {
+        int pivot = array[right];
 
         int i = left;
         int j = right - 1;
         while (i < j) {
             // Find the first element >= pivot
-            while (elements[i] < pivot) {
+            while (array[i] < pivot) {
                 i++;
             }
 
             // Find the last element < pivot
-            while (j > left && elements[j] >= pivot) {
+            while (j > left && array[j] >= pivot) {
                 j--;
             }
 
             // If the greater element is left of the lesser element, switch them
             if (i < j) {
-                //ArrayUtils.swap(elements, i, j);
-                int temp = elements[i];
-                elements[i] = elements[j];
-                elements[j] = temp;
+                //ArrayUtils.swap(array, i, j);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
                 i++;
                 j--;
             }
@@ -70,16 +70,16 @@ public class Quicksort {
 
         // i == j means we haven't checked this index yet.
         // Move i right if necessary so that i marks the start of the right array.
-        if (i == j && elements[i] < pivot) {
+        if (i == j && array[i] < pivot) {
             i++;
         }
 
         // Move pivot element to its final position
-        if (elements[i] != pivot) {
-            //ArrayUtils.swap(elements, i, right);
-            int temp = elements[i];
-            elements[i] = elements[right];
-            elements[right] = temp;
+        if (array[i] != pivot) {
+            //ArrayUtils.swap(array, i, right);
+            int temp = array[i];
+            array[i] = array[right];
+            array[right] = temp;
         }
         return i;
     }
